be6b7f726a26c5c57c8b3994130ac2b2163e7e59
FullBarbosa <diibarbosa22@gmail.com>
2021-06-30 17:10:47 -0300

    terceiro dia

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 
-import {BrowserRouter,Route} from 'react-router-dom'
-
-import { Home } from './pages/Home'
-import { NewRoom } from './pages/NewRoom';
-
+import {BrowserRouter,Route, Switch} from 'react-router-dom'
 import {AuthContextProvider} from '../src/contexts/AuthContext'
 
 import './services/firebase';
 import './styles/global.scss';
+
+import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom';
+import { Room } from './pages/Room';
+
+
 
 export default function App() {
 
   return (
     <BrowserRouter>
     <AuthContextProvider>
+      <Switch>
         <Route path="/" exact component={Home}/>
         <Route path="/rooms/new" component={NewRoom}/>
+        <Route path="/rooms/:id" component={Room}/>
+      </Switch>
     </AuthContextProvider>
     </BrowserRouter>
   )
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,15 +1,37 @@
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
+import { FormEvent, useState } from 'react';
+import { database } from '../services/firebase';
 import { useAuth } from '../hooks/useAuth';
+
 
 export function NewRoom() {
 
-  const {user} = useAuth()
+  const {user} = useAuth();
+  const [newRoom, setNewRoom] = useState(''); 
+  const history = useHistory();
+
+ async function handleCreateRoom(event:FormEvent){
+   event.preventDefault();
+
+   if(newRoom.trim() === ''){
+    return;
+   }
+
+   const roomRef = database.ref('rooms');
+
+   const firebaseRoom = await roomRef.push({
+     title: newRoom,
+     authorId: user?.id,
+   })
+
+   history.push(`/room/${firebaseRoom.key}`)
+ }
 
   return (
     <div id="page-auth">
@@ -24,12 +46,13 @@ export function NewRoom() {
             src={logoImg}
             alt="letmeask"
           />
-          <h1>{user?.name}</h1>
+        
           <h2>Criar um nova sala</h2>
-          <form >
+          <form onSubmit={handleCreateRoom} >
             <input
               type="text"
               placeholder="Home on sala"
+              onChange={event => setNewRoom(event.target.value)}
             />
             <Button type="submit">
               Criar sala
